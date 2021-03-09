c21b09f28ee5aa35d54e1d8a6e2b90a342022e94
Tirth Patel <tirrrthpatel@gmail.com>
2021-03-09 22:51:44 +0530

    Added a view page for health-check API

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/health-check', function(req, res, next) {
-  res.render('index', { title: 'TelePlayer Bot Health Check' });
+  res.render('health-check', { title: 'TelePlayer Bot Health Check' });
 });
 
 module.exports = router;
