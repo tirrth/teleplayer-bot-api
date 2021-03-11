a50417e55c58f9699e7e7c95bf36c259cca4d00d
Tirth Patel <tirrrthpatel@gmail.com>
2021-03-11 09:04:38 +0530

    Added index route

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'TelePlayer Bot' });
+});
+
 router.get('/health-check', function(req, res, next) {
   res.render('health-check', { title: 'TelePlayer Bot Health Check' });
 });
